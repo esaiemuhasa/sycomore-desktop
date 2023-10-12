1be20e9e92b9a29c32bc40f4d165eca84e3f55f6
Esaie Muhasa <esaiemuhasa.dev@gmail.com>
2023-10-12 08:28:19 +0100

    Implementation du formulaire de saisie des sections, options, et classe d'etudes

---
+++ src/main/java/com/sycomore/view/moreoption/ClassifiableForm.java
@@ -0,0 +1,90 @@
+package com.sycomore.view.moreoption;
+
+import com.sycomore.entity.Classifiable;
+import com.sycomore.view.componets.TextFieldWrapper;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ClassifiableForm <T extends Classifiable> extends JPanel {
+
+    private final TextFieldWrapper shortNameWrapper = new TextFieldWrapper("Abbreviation", "");
+    private final TextFieldWrapper fullNameWrapper = new TextFieldWrapper("Appellation complete", "");
+
+    private final JButton buttonValidate = new JButton("Sauvegarder");
+    private final JButton buttonCancel = new JButton("Annuler");
+
+    private final ClassifiableFormListener formListener;
+    private T data;
+
+    public ClassifiableForm (ClassifiableFormListener formListener) {
+        super(new BorderLayout());
+        this.formListener = formListener;
+        init();
+    }
+
+    public void setData(T data) {
+        this.data = data;
+
+        razFields();
+
+        if (data != null) {
+            if (data.getShortName() != null)
+                shortNameWrapper.getField().setText(data.getShortName());
+
+            if (data.getFullName() != null)
+                fullNameWrapper.getField().setText(data.getFullName());
+        }
+    }
+
+    private void init () {
+        JPanel footer = new JPanel();
+        Box center = Box.createVerticalBox();
+
+        center.add(shortNameWrapper);
+        center.add(fullNameWrapper);
+
+        footer.add(buttonValidate);
+        footer.add(buttonCancel);
+
+        add(center, BorderLayout.CENTER);
+        add(footer, BorderLayout.SOUTH);
+
+        setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+        buttonValidate.addActionListener(e -> {
+            String shortName = shortNameWrapper.getField().getText();
+            String fullName = fullNameWrapper.getField().getText();
+
+            formListener.onValidate(shortName, fullName, data);
+            razFields();
+        });
+
+        buttonCancel.addActionListener(e -> {
+            formListener.onCancel();
+            razFields();
+        });
+    }
+
+    private void razFields () {
+        shortNameWrapper.getField().setText("");
+        fullNameWrapper.getField().setText("");
+    }
+
+
+    /**
+     * Interface d'écoute de changement d'etat du formulaire de saisi
+     */
+    public interface ClassifiableFormListener {
+
+        /**
+         * Lorsque l'utilisateur valide l'opération
+         */
+        void onValidate (String shortName, String fullName, Classifiable data);
+
+        /**
+         * Lorsque l'utilisateur annule l'opération
+         */
+        void onCancel ();
+    }
+}
--- src/main/java/com/sycomore/view/moreoption/SectionsPanel.java
+++ src/main/java/com/sycomore/view/moreoption/SectionsPanel.java
@@ -1,11 +1,18 @@
 package com.sycomore.view.moreoption;
 
+import com.sycomore.dao.DAOFactory;
+import com.sycomore.dao.OptionRepository;
+import com.sycomore.dao.SectionRepository;
+import com.sycomore.entity.Classifiable;
+import com.sycomore.entity.Option;
 import com.sycomore.entity.Section;
 import com.sycomore.model.OptionTableModel;
 import com.sycomore.model.SectionComboBoxModel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.util.Date;
 
 public class SectionsPanel extends JPanel {
 
@@ -18,9 +25,21 @@ public class SectionsPanel extends JPanel {
     private final JButton buttonAddingSection = new JButton("Ajouter une section");
     private final JButton buttonAddingOption = new JButton("Ajouter une option");
 
+    private JDialog dialog;
+    private ClassifiableForm <Classifiable> classifiableForm;
+
+    private final SectionRepository sectionRepository;
+    private final OptionRepository optionRepository;
+
     public SectionsPanel() {
         super(new BorderLayout(10, 10));
+
+        sectionRepository = DAOFactory.getInstance(SectionRepository.class);
+        optionRepository = DAOFactory.getInstance(OptionRepository.class);
         init();
+
+
+        boxModel.init();
     }
 
     private void init () {
@@ -36,5 +55,79 @@ public class SectionsPanel extends JPanel {
 
         add(head, BorderLayout.NORTH);
         add(table, BorderLayout.CENTER);
+
+
+        buttonAddingSection.addActionListener(e -> {
+            buildDialog();
+
+            dialog.setLocationRelativeTo(this);
+            classifiableForm.setData(new Section());
+
+            dialog.setVisible(true);
+        });
+
+        buttonAddingOption.addActionListener(e -> {
+            buildDialog();
+
+            dialog.setLocationRelativeTo(this);
+            classifiableForm.setData(new Option());
+
+            dialog.setVisible(true);
+        });
+
+        comboBox.addItemListener(event -> {
+            if (event.getStateChange() == ItemEvent.SELECTED) {
+                tableModel.setSection(boxModel.getElementAt(comboBox.getSelectedIndex()));
+            }
+        });
     }
+
+    private void buildDialog () {
+        if (dialog != null)
+            return;
+
+        dialog = new JDialog();
+        classifiableForm = new ClassifiableForm<>(formListener);
+
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        dialog.setContentPane(classifiableForm);
+        dialog.pack();
+        dialog.setSize(dialog.getWidth() + 200, dialog.getHeight());
+        dialog.setResizable(false);
+    }
+
+    private final ClassifiableForm.ClassifiableFormListener formListener = new ClassifiableForm.ClassifiableFormListener() {
+        @Override
+        public void onValidate(String shortName, String fullName, Classifiable data) {
+            if (data == null)
+                return;
+
+            dialog.setVisible(false);
+            dialog.dispose();
+
+            data.setShortName(shortName);
+            data.setFullName(fullName);
+
+            if (data.getRecordingDate() == null)
+                data.setRecordingDate(new Date());
+            else
+                data.setUpdatingDate(new Date());
+
+            if (data instanceof Option) {
+                Option option = (Option) data;
+                Section section = boxModel.getElementAt(comboBox.getSelectedIndex());
+                option.setSection(section);
+                optionRepository.persist(option);
+            } else {
+                Section section = (Section) data;
+                sectionRepository.persist(section);
+            }
+        }
+
+        @Override
+        public void onCancel() {
+            dialog.setVisible(false);
+            dialog.dispose();
+        }
+    };
 }
