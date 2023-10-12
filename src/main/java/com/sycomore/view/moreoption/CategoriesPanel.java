4af95117c2e15b782d230f7da80af4914dab8f63
Esaie Muhasa <esaiemuhasa.dev@gmail.com>
2023-10-12 10:50:59 +0100

    Implementation du formulaire de categorisation des classes d'etudes

--- src/main/java/com/sycomore/view/moreoption/CategoriesPanel.java
+++ src/main/java/com/sycomore/view/moreoption/CategoriesPanel.java
@@ -1,6 +1,149 @@
 package com.sycomore.view.moreoption;
 
+import com.sycomore.dao.CategoryRepository;
+import com.sycomore.dao.DAOFactory;
+import com.sycomore.entity.Category;
+import com.sycomore.entity.School;
+import com.sycomore.model.CategoryTableModel;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Date;
 
 public class CategoriesPanel extends JPanel {
+
+    private final CategoryTableModel tableModel = new CategoryTableModel();
+    private final JTable table = new JTable(tableModel);
+    private final JPopupMenu popup = new JPopupMenu();
+    private final JMenuItem update = new JMenuItem("Modifier");
+    private final JMenuItem delete = new JMenuItem("Supprimer");
+    private final JButton buttonAdding = new JButton("Ajouter");
+
+    private final CategoryRepository categoryRepository;
+
+    private JDialog dialog;//boite de dialogue de manipulation des infos d'une catégorie
+    private CategoryForm categoryForm;
+
+    public CategoriesPanel() {
+        super (new BorderLayout(5, 5));
+        categoryRepository = DAOFactory.getInstance(CategoryRepository.class);
+        init();
+    }
+
+    private Category getSelectedItem () {
+        return tableModel.getRow(table.getSelectedRow());
+    }
+
+    private void init () {
+        JPanel top = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        JPanel center = new JPanel(new BorderLayout());
+
+        top.add(buttonAdding);
+        center.add(table);
+        center.setBorder(BorderFactory.createLineBorder(UIManager.getColor("border_color")));
+
+        add(top, BorderLayout.NORTH);
+        add(center, BorderLayout.CENTER);
+
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        table.addMouseListener(mouseListener);
+        buttonAdding.addActionListener(event -> doAdding());
+
+        //popup menu
+        popup.add(update);
+        popup.add(delete);
+        //==
+
+        update.addActionListener(e -> doUpdating());
+        delete.addActionListener(e -> doDelete());
+    }
+
+    /**
+     * Ouverture de la boite de dialogue d'insertion d'une nouvelle catégorie
+     */
+    private void doAdding ()  {
+        buildDialog();
+
+        dialog.setTitle("Insertion d'une catégorie");
+        dialog.setLocationRelativeTo(this);
+        dialog.setVisible(true);
+    }
+
+    /**
+     * Action de mis en jours du profil d'une catégorie
+     */
+    private void doUpdating () {
+        buildDialog();
+
+        categoryForm.setCategory(getSelectedItem());
+        dialog.setLocationRelativeTo(this);
+        dialog.setTitle("Modification de l'appellation");
+        dialog.setVisible(true);
+    }
+
+    /**
+     * Action de suppression d'une école
+     */
+    private void doDelete () {
+        Category category = getSelectedItem();
+        int status = JOptionPane.showConfirmDialog(this, "Voulez-vous vraiment supprimer la catégorie?\n"
+                        +"\""+category.getLabel()+"\"",
+                "Suppression", JOptionPane.OK_CANCEL_OPTION);
+
+        if (status == JOptionPane.OK_OPTION) {
+            categoryRepository.remove(category);
+        }
+    }
+
+    private void buildDialog () {
+        if (dialog != null)
+            return;
+
+        dialog = new JDialog();
+        categoryForm = new CategoryForm(formListener);
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        dialog.setContentPane(categoryForm);
+        dialog.pack();
+        dialog.setSize(dialog.getWidth() + 200, dialog.getHeight()+10);
+        dialog.setResizable(false);
+    }
+
+    private final MouseListener mouseListener = new MouseAdapter() {
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            if (!e.isPopupTrigger() || table.getSelectedRowCount() == 0)
+                return;
+
+            popup.show(table, e.getX(), e.getY());
+        }
+    };
+
+
+    private final CategoryForm.CategoryFormListener formListener = new CategoryForm.CategoryFormListener() {
+        @Override
+        public void onValidate(String value, Category category) {
+            dialog.setVisible(false);
+            dialog.dispose();
+
+            if (category == null)
+                category = new Category();
+
+            category.setLabel(value);
+            if (category.getRecordingDate() == null)
+                category.setRecordingDate(new Date());
+            else
+                category.setUpdatingDate(new Date());
+
+            categoryRepository.persist(category);
+        }
+
+        @Override
+        public void onCancel() {
+            dialog.setVisible(false);
+            dialog.dispose();
+        }
+    };
 }
---
+++ src/main/java/com/sycomore/view/moreoption/CategoryForm.java
@@ -0,0 +1,62 @@
+package com.sycomore.view.moreoption;
+
+import com.sycomore.entity.Category;
+import com.sycomore.view.componets.TextFieldWrapper;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CategoryForm extends JPanel {
+
+    private final JButton buttonValidate = new JButton("Valider");
+    private final JButton buttonCancel = new JButton("Annuler");
+
+    private final TextFieldWrapper fieldWrapper = new TextFieldWrapper("Appellation", "");
+
+    private final CategoryFormListener formListener;
+
+    private Category category;//catégorie en cours de modification
+
+    public CategoryForm(CategoryFormListener formListener) {
+        super(new BorderLayout(10, 10));
+        this.formListener = formListener;
+        init();
+    }
+
+    private void init () {
+        JPanel footer = new JPanel();
+
+        footer.add(buttonValidate);
+        footer.add(buttonCancel);
+
+        add(footer, BorderLayout.SOUTH);
+        add(fieldWrapper, BorderLayout.CENTER);
+        setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        buttonCancel.addActionListener(e -> {
+            fieldWrapper.getField().setText("");
+            formListener.onCancel();
+        });
+
+        buttonValidate.addActionListener(e -> {
+            String value = fieldWrapper.getField().getText();
+            fieldWrapper.getField().setText("");
+            formListener.onValidate(value, category);
+            category = null;
+        });
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+        String label = (category != null && category.getLabel() != null) ? category.getLabel() : "";
+        fieldWrapper.getField().setText(label);
+    }
+
+    /**
+     * Interface d'écoute des évènements
+     */
+    public interface CategoryFormListener {
+        void onValidate(String value, Category category);
+        void onCancel();
+    }
+}
