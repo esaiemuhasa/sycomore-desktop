36bf9192aeb835abb3a388fa9ebee6fcd71b31aa
Esaie Muhasa <esaiemuhasa.dev@gmail.com>
2023-10-16 13:06:30 +0100

    Mis en marche du formulaire d'insertion dela config des frais scolaires

--- src/main/java/com/sycomore/view/workspace/control/StudyFeesConfigForm.java
+++ src/main/java/com/sycomore/view/workspace/control/StudyFeesConfigForm.java
@@ -11,13 +11,15 @@ import java.util.Date;
 public class StudyFeesConfigForm extends JPanel {
 
     private final TextFieldWrapper amountWrapper = new TextFieldWrapper("Montant (en USD)", "1");
-    private final DateFieldWrapper startDate = new DateFieldWrapper("Début de recouvrement");
-    private final DateFieldWrapper endDate = new DateFieldWrapper("Find de recouvrement");
+    private final DateFieldWrapper startDateWrapper = new DateFieldWrapper("Début de recouvrement");
+    private final DateFieldWrapper endDateWrapper = new DateFieldWrapper("Find de recouvrement");
     private final TextFieldWrapper descriptionWrapper = new TextFieldWrapper("Courte description", "");
 
     private final JButton buttonValidate = new JButton("Sauvegarder");
     private final JButton buttonCancel = new JButton("Annuler");
     private final StudyFeesConfigFormListener formListener;
+
+    private StudyFeesConfig config;
 
     public StudyFeesConfigForm(StudyFeesConfigFormListener formListener) {
         super(new BorderLayout());
@@ -31,8 +33,8 @@ public class StudyFeesConfigForm extends JPanel {
         JPanel bottom = new JPanel();
         JPanel container = new JPanel(new BorderLayout(10, 10));
 
-        dates.add(startDate);
-        dates.add(endDate);
+        dates.add(startDateWrapper);
+        dates.add(endDateWrapper);
 
         container.add(amountWrapper, BorderLayout.NORTH);
         container.add(dates, BorderLayout.CENTER);
@@ -46,14 +48,42 @@ public class StudyFeesConfigForm extends JPanel {
 
         buttonValidate.addActionListener(e -> handleValidate());
         buttonCancel.addActionListener(e -> handleCancel());
+        setBorder(BorderFactory.createEmptyBorder(10, 10,10, 10));
+    }
+
+    public void setConfig(StudyFeesConfig config) {
+        this.config = config;
+
+        if (config != null){
+            amountWrapper.getField().setText(String.valueOf(config.getAmount()));
+            descriptionWrapper.getField().setText(config.getCaption());
+            startDateWrapper.getField().setDate(config.getStart());
+            endDateWrapper.getField().setDate(config.getEnd());
+        } else {
+            razField();
+        }
+    }
+
+    private void razField () {
+        amountWrapper.getField().setText("1");
+        descriptionWrapper.getField().setText("");
+        startDateWrapper.getField().setDate(new Date());
+        endDateWrapper.getField().setDate(new Date(System.currentTimeMillis() + (60L * 1000L * 24L * 30L)));
     }
 
     private void handleValidate () {
+        double amount = Double.parseDouble(amountWrapper.getField().getText());
+        Date start = startDateWrapper.getField().getDate();
+        Date end = endDateWrapper.getField().getDate();
+        String caption = descriptionWrapper.getField().getText();
 
+        formListener.onValidate(amount, start, end, caption, config);
+        config = null;
     }
 
     private void handleCancel () {
-
+        razField();
+        formListener.onCancel();
     }
 
     
--- src/main/java/com/sycomore/view/workspace/control/StudyFeesPanel.java
+++ src/main/java/com/sycomore/view/workspace/control/StudyFeesPanel.java
@@ -2,6 +2,7 @@ package com.sycomore.view.workspace.control;
 
 import com.sycomore.dao.DAOFactory;
 import com.sycomore.dao.PromotionStudyFeesRepository;
+import com.sycomore.dao.RepositoryAdapter;
 import com.sycomore.dao.StudyFeesConfigRepository;
 import com.sycomore.entity.PromotionStudyFees;
 import com.sycomore.entity.School;
@@ -9,9 +10,13 @@ import com.sycomore.entity.StudyFeesConfig;
 import com.sycomore.model.YearDataModel;
 import com.sycomore.model.YearDataModelAdapter;
 import com.sycomore.model.YearDataModelListener;
+import com.sycomore.view.MainWindow;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Date;
 
 /**
  * Panel de configuration des frais d'étude pour une année scolaire
@@ -28,6 +33,13 @@ public class StudyFeesPanel extends JPanel {
     private final JButton buttonAddingConfig = new JButton("Ajouter un autre frais");
     private final JButton buttonAddingPromotion = new JButton("Associer d'autre(s) promotion(s)");
 
+    private final JPopupMenu configPopup = new JPopupMenu();
+    private final JMenuItem itemDeleteConfig = new JMenuItem("Supprimer");
+    private final JMenuItem itemUpdateConfig = new JMenuItem("Modifier");
+
+    private JDialog configDialog;
+    private StudyFeesConfigForm configForm;
+
     private final YearDataModel dataModel;
     private final StudyFeesConfigRepository studyFeesConfigRepository;
     private final PromotionStudyFeesRepository promotionStudyFeesRepository;
@@ -41,6 +53,42 @@ public class StudyFeesPanel extends JPanel {
         init();
 
         dataModel.addYearDataListener(dataModelListener);
+
+        studyFeesConfigRepository.addRepositoryListener(new RepositoryAdapter<StudyFeesConfig>() {
+            @Override
+            public void onCreate(StudyFeesConfig config) {
+                if (config.getYear().equals(dataModel.getYear()))
+                    listModelConfig.addElement(config);
+            }
+
+            @Override
+            public void onUpdate(StudyFeesConfig oldState, StudyFeesConfig newState) {
+                if (!newState.getYear().equals(dataModel.getYear()))
+                    return;
+
+                for (int i = 0; i < listModelConfig.getSize(); i++) {
+                    StudyFeesConfig item = listModelConfig.getElementAt(i);
+                    if (item.equals(newState)){
+                        listModelConfig.setElementAt(item, i);
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onDelete(StudyFeesConfig config) {
+                if (!config.getYear().equals(dataModel.getYear()))
+                    return;
+
+                for (int i = 0; i < listModelConfig.getSize(); i++) {
+                    StudyFeesConfig item = listModelConfig.getElementAt(i);
+                    if (item.equals(config)){
+                        listModelConfig.remove(i);
+                        break;
+                    }
+                }
+            }
+        });
     }
 
     private void init () {
@@ -73,6 +121,19 @@ public class StudyFeesPanel extends JPanel {
 
         add(split, BorderLayout.CENTER);
         setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        configPopup.add(itemUpdateConfig);
+        configPopup.add(itemDeleteConfig);
+
+        buttonAddingConfig.addActionListener(e -> handleAddingConfiguration());
+        itemUpdateConfig.addActionListener(e -> handleUpdateConfiguration());
+        listConfig.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (listConfig.getSelectedIndex() != -1 && e.isPopupTrigger())
+                    configPopup.show(listConfig, e.getX(), e.getY());
+            }
+        });
     }
 
     private void revalidateSchools () {
@@ -85,10 +146,77 @@ public class StudyFeesPanel extends JPanel {
             comboBoxModel.addElement(s);
     }
 
+    private StudyFeesConfig getSelectedConfig () {
+        return listModelConfig.getElementAt(listConfig.getSelectedIndex());
+    }
+
+    private void handleAddingConfiguration () {
+        buildConfigDialog();
+
+        configDialog.setLocationRelativeTo(configDialog.getOwner());
+        configDialog.setTitle("Ajout d'une configuration des frais scolaire");
+        configDialog.setVisible(true);
+    }
+
+    private void handleUpdateConfiguration () {
+        buildConfigDialog();
+
+        StudyFeesConfig config = getSelectedConfig();
+        configForm.setConfig(config);
+        configDialog.setLocationRelativeTo(configDialog.getOwner());
+        configDialog.setTitle("Modification de la configuration des frais scolaire");
+        configDialog.setVisible(true);
+    }
+
+    private void buildConfigDialog () {
+        if (configDialog != null)
+            return;
+
+        configDialog = new JDialog(MainWindow.getInstance());
+        configForm = new StudyFeesConfigForm(configFormListener);
+
+        configDialog.setContentPane(configForm);
+        configDialog.pack();
+        configDialog.setSize(configDialog.getWidth() + 200, configDialog.getHeight());
+        configDialog.setResizable(false);
+        configDialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    }
+
     protected final YearDataModelListener dataModelListener = new YearDataModelAdapter() {
         @Override
         public void onLoadFinish() {
             revalidateSchools();
+        }
+    };
+
+    protected final StudyFeesConfigForm.StudyFeesConfigFormListener configFormListener = new StudyFeesConfigForm.StudyFeesConfigFormListener() {
+        @Override
+        public void onValidate(double amount, Date startDate, Date endDate, String caption, StudyFeesConfig config) {
+            onCancel();
+
+            if (config == null)
+                config = new StudyFeesConfig();
+
+            config.setAmount(amount);
+            config.setStart(new java.sql.Date(startDate.getTime()));
+            config.setEnd(new java.sql.Date(endDate.getTime()));
+            config.setCaption(caption);
+
+            if (config.getId() == null || config.getId() <= 0) {
+                config.setYear(dataModel.getYear());
+                config.setSchool(comboBoxModel.getElementAt(comboBox.getSelectedIndex()));
+                config.setRecordingDate(new Date());
+            } else {
+                config.setUpdatingDate(new Date());
+            }
+
+            studyFeesConfigRepository.persist(config);
+        }
+
+        @Override
+        public void onCancel() {
+            configDialog.setVisible(false);
+            configDialog.dispose();
         }
     };
 
